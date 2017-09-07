667e9e90cc59029520a06fb1c1f479c12856a50f
sankarprasad <sankarprasadpanda5@gmail.com>
2017-09-07 11:06:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 var bodyParser = require('body-parser');
